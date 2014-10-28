de94ebafacdffbf935b32e4d14d74ae7c9ad702b
Jon Douglas <jondouglas18@gmail.com>
2014-10-28 15:13:32 -0600

    Added AdditiveBlending on SpriteBatches

---
+++ AdditiveBlending/Game1.cs
@@ -0,0 +1,134 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace AdditiveBlending
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Texture2D blue;
+        private Texture2D green;
+        private Texture2D red;
+
+
+        private float blueAngle = 0;
+        private float greenAngle = 0;
+        private float redAngle = 0;
+
+        private float blueSpeed = 0.025f;
+        private float greenSpeed = 0.017f;
+        private float redSpeed = 0.022f;
+
+        private float distance = 100;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            blue = Content.Load<Texture2D>("blue");
+            green = Content.Load<Texture2D>("green");
+            red = Content.Load<Texture2D>("red");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+            blueAngle += blueSpeed;
+            greenAngle += greenSpeed;
+            redAngle += redSpeed;
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            Vector2 bluePosition = new Vector2(
+                (float)Math.Cos(blueAngle) * distance,
+                (float)Math.Sin(blueAngle) * distance);
+            Vector2 greenPosition = new Vector2(
+                            (float)Math.Cos(greenAngle) * distance,
+                            (float)Math.Sin(greenAngle) * distance);
+            Vector2 redPosition = new Vector2(
+                            (float)Math.Cos(redAngle) * distance,
+                            (float)Math.Sin(redAngle) * distance);
+
+            Vector2 center = new Vector2(300, 140);
+
+
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive);
+
+            spriteBatch.Draw(blue, center + bluePosition, Color.White);
+            spriteBatch.Draw(green, center + greenPosition, Color.White);
+            spriteBatch.Draw(red, center + redPosition, Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
