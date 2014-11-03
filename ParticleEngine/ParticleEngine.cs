29226b986fe02bfc51869cff79b4bdaed62338f3
Jon Douglas <Douglasjm@live.com>
2014-11-02 18:53:11 -0700

    Added ParticleEngine

---
+++ ParticleEngine/ParticleEngine.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ParticleEngine
+{
+    public class ParticleEngine
+    {
+        private Random random;
+        public Vector2 EmitterLocation { get; set; }
+        private List<Particle> Particles;
+        private List<Texture2D> Textures;
+
+        public ParticleEngine(List<Texture2D> textures, Vector2 location)
+        {
+            EmitterLocation = location;
+            Textures = textures;
+            Particles = new List<Particle>();
+            random = new Random();
+        }
+
+        private Particle GenerateNewParticle()
+        {
+            Texture2D texture = Textures[random.Next(Textures.Count)];
+            Vector2 position = EmitterLocation;
+            Vector2 velocity = new Vector2(1f * (float)(random.NextDouble() * 2 - 1), 1f * (float)(random.NextDouble() * 2 - 1));
+            float angle = 0;
+            float angularVelocity = 0.1f*(float) (random.NextDouble()*2 - 1);
+            Color color = new Color((float)random.NextDouble(), (float)random.NextDouble(), (float)random.NextDouble());
+            float size = (float) random.NextDouble();
+            int ttl = 20 + random.Next(40);
+
+            return new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl);
+        }
+
+        public void Update()
+        {
+            int total = 10;
+
+            for (int i = 0; i < total; i++)
+            {
+                Particles.Add(GenerateNewParticle());
+            }
+
+            for (int particle = 0; particle < Particles.Count; particle++)
+            {
+                Particles[particle].Update();
+                if (Particles[particle].TTL <= 0)
+                {
+                    Particles.RemoveAt(particle);
+                    particle--;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            for (int index = 0; index < Particles.Count; index++)
+            {
+                Particles[index].Draw(spriteBatch);
+            }
+            spriteBatch.End();
+        }
+
+    }
+}
