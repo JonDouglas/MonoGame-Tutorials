1b8e0184c6e7154fac76c17e45de2f1172ee6a8d
Jon Douglas <Douglasjm@live.com>
2014-11-02 17:54:30 -0700

    Texture Atlasses added

---
+++ TextureAtlases/AnimatedSprite.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TextureAtlases
+{
+    public class AnimatedSprite
+    {
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public int currentFrame;
+        public int totalFrames;
+
+        public AnimatedSprite(Texture2D texture, int rows, int columns)
+        {
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows*Columns;
+        }
+
+        public void Update()
+        {
+            currentFrame++;
+
+            if (currentFrame == totalFrames)
+            {
+                currentFrame = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        {
+            int width = Texture.Width/Columns;
+            int height = Texture.Height/Rows;
+            int row = (int) ((float) currentFrame/(float) Columns);
+            int column = currentFrame%Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
